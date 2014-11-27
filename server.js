ecba3235023018368ffa566a9dd7ac933f6e2f66
Yaopeng Wu <yaopeng.wu@sjsu.edu>
2014-11-27 08:23:09 -0800

    starter code

--- server.js
+++ server.js
@@ -4,11 +4,28 @@ var express = require('express');
 var Client = require('node-rest-client').Client;
 var port = process.env.PORT || 8080;
 
-// CONFIGURE APP
+
+// configure app to use the packages we pulled in using npm
 var app = express();
 app.use(express.bodyParser());
 app.use("/images", express.static(__dirname + '/images'));
 
 
+// =============================================================================
+var router = express.Router();
+
+// accessed at GET http://localhost:8080/api
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
 // START THE SERVER
 app.listen(port);
+console.log('Magic happens on port ' + port);
